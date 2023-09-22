6a1a78806814284b21ccfdb076abd3f09b6d56c6
yazilimkodlama <145126274+yazilimkodlama@users.noreply.github.com>
2023-09-22 15:29:53 +0300

    Add files via upload

--- Bolum1Uygulama5/Form1.Designer.cs
+++ Bolum1Uygulama5/Form1.Designer.cs
@@ -45,6 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(545, 221);
             this.Controls.Add(this.btnMesaj);
             this.Name = "Form1";
